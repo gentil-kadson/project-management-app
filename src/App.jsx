5e0e4619b17116b14cc78e161fb193478adb819e
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-05-09 11:40:35 -0300

    conditionally renders the add new project form when clicking either the button from the sidebar or the one from the 'No project selected' content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSiderbar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
-    currentProject: undefined,
+    selectedProjectId: undefined,
     projects: [],
   });
 
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) content = <NewProject />;
+  if (projectsState.selectedProjectId === undefined)
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSiderbar />
+      <ProjectsSiderbar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
