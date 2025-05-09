3b1e1cf9f80c184d6842e8d1c0d2caaa299f56fb
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-05-09 11:06:59 -0300

    creates project's State.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSiderbar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    currentProject: undefined,
+    projects: [],
+  });
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSiderbar />
-      <NewProject />
     </main>
   );
 }
